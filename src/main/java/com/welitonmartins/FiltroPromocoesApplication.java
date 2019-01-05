234ab0e1edf5b758ec32d9a9f29e42353cd5064e
Weliton Alves Martins <weliton.martins@yaman.com.br>
2019-01-05 17:57:19 -0200

    primeiro commit, configuracoes do projeto e incluindo template

---
+++ src/main/java/com/welitonmartins/FiltroPromocoesApplication.java
@@ -0,0 +1,14 @@
+package com.welitonmartins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltroPromocoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltroPromocoesApplication.class, args);
+	}
+
+}
+
