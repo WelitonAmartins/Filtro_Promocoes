3d81f6bdcdc82ae37a2807de98240a5b32f91432
Weliton Alves Martins <weliton.martins@yaman.com.br>
2019-01-05 23:29:16 -0200

    fazendo tratamento de erro 404 e 500, com jquery e limpando campos quando não tiver um url valida

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -8,10 +8,26 @@ $("#linkPromocao").on('change', function(){
 			method:"POST",
 			url:"/meta/info?url=" + url,
 			cache: false,
+			beforeSend: function() {
+				$("#alert").removeClass("alert alert-danger").text("");
+				$("#title").val("");
+				$("#site").text("");
+				$("#linkImagem").attr("src", "");
+			},
 			success: function( data ) {
 				console.log(data);
+				$("#titulo").val(data.title);
+				$("#site").text(data.site.replace("@", ""));
+				$("#linkImagem").attr("src", data.image);
+			},
+			statusCode: {
+				404: function() {
+					$("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa url.");
+				}
+			},
+			error: function() {
+				$("#alert").addClass("alert alert-danger").text("Ops ... Algo deu errado, tente novamente mais tarde.");
 			}
-			
-		})
+		});
 	}
-})
+});
