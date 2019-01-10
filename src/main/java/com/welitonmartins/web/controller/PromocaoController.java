41b373ebb22d18f97f994b55ac37835ce625170e
Weliton Alves Martins <weliton.martins@yaman.com.br>
2019-01-09 22:06:27 -0200

    contruindo query para update e busca dos likes e criando metado controller que manda o response

--- src/main/java/com/welitonmartins/web/controller/PromocaoController.java
+++ src/main/java/com/welitonmartins/web/controller/PromocaoController.java
@@ -19,6 +19,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,7 +58,15 @@ public class PromocaoController {
 		return "promo-card";
 	}
 	
+	//==================== ADICIONA LIKES ===========================================
 	
+	@PostMapping("/like/{id}")
+	public ResponseEntity<?> adicionarLikes(@PathVariable("id") Long id){
+		promocaoRepository.updateSomarLikes(id);
+		int likes = promocaoRepository.findLikesById(id);
+		return ResponseEntity.ok(likes);
+		
+	}
 	
 	
 	//============================ ADICIONA OFERTA ====================================
