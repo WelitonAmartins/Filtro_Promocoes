8815ffb602a1ff8546e4867d120d57dc4e7d653a
Weliton Alves Martins <weliton.martins@yaman.com.br>
2019-01-10 21:19:53 -0200

    incluindo metado no jquery de autocomplete

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -57,6 +57,22 @@ function loadByScrollBar(pageNumber) {
 	})  
 }
 
+// autocomplete
+$("#autocomplete-input").autocomplete({
+	source: function(request, response) {
+		$.ajax({
+			method: "GET",
+			url: "/promocao/site",
+			data: {
+				termo: request.term
+			},
+			sucess: function(result) {
+				response(result);
+			}
+		})
+	}
+})
+
 // adicionar likes
  
 $(document).on("click", "button[id*='likes-btn-']", function(){
