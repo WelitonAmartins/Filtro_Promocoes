b79b9f09c496ea53102fcdad05ffc0df6033ec31
Weliton Alves Martins <weliton.martins@yaman.com.br>
2019-01-10 22:31:07 -0200

    criando metado de consulta da query no repository e metado controller que verificar a promocao desejada no autocomplite

--- src/main/java/com/welitonmartins/web/controller/PromocaoController.java
+++ src/main/java/com/welitonmartins/web/controller/PromocaoController.java
@@ -51,11 +51,36 @@ public class PromocaoController {
 	}
 	
 	@GetMapping("/list/ajax")
-	public String listarCards(@RequestParam(name ="page", defaultValue = "1") int page, ModelMap model) {
+	public String listarCards(@RequestParam(name ="page", defaultValue = "1") int page,
+							  @RequestParam(name ="site", defaultValue = "") String site,
+																			ModelMap model) {
+		
 		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
 		PageRequest pageRequest = PageRequest.of(page, 8, sort);
-		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		
+		if(site.isEmpty()) {
+			model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		}else {
+			model.addAttribute("promocoes", promocaoRepository.findBySite(site, pageRequest));
+
+		}
 		return "promo-card";
+	}
+	
+	// =================== AUTOCOMPLETE=====================================
+	
+	@GetMapping("/site")
+	public ResponseEntity<?> autocompleteByTermo(@RequestParam("termo") String termo){
+		List<String> sites = promocaoRepository.findSiteByTermo(termo);
+		return ResponseEntity.ok(sites);
+	}
+	
+	@GetMapping("/site/list")
+	public String listarPorSite(@RequestParam("site") String site, ModelMap model) {
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findBySite(site, pageRequest));
+		return "promo-card"; 
 	}
 	
 	//==================== ADICIONA LIKES ===========================================
